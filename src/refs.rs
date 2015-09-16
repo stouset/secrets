7b9ffe325f2a9b297eae40fbf888234223a2cdf7
Stephen Touset <stephen@touset.org>
2015-09-16 10:55:08 -0700

    Docs on Ref and RefMut

--- src/refs.rs
+++ src/refs.rs
@@ -3,11 +3,21 @@ use sec::Sec;
 use std::borrow::{Borrow, BorrowMut};
 use std::ops::{Deref, DerefMut};
 
+/// Wraps an immutably borrowed reference to the contents of a `Secret`.
+///
+/// The contents of the `Secret` can be accessed through this `Ref`
+/// via slice or pointer semantics. See the documentation for `Secret`
+/// for details.
 #[derive(Debug)]
 pub struct Ref<'a, T: 'a> {
     sec: &'a Sec<T>,
 }
 
+/// Wraps an mutably borrowed reference to the contents of a `Secret`.
+///
+/// The contents of the `Secret` can be accessed through this `RefMut`
+/// via slice or pointer semantics. See the documentation for `Secret`
+/// for details.
 #[derive(Debug)]
 pub struct RefMut<'a, T: 'a> {
     sec: &'a mut Sec<T>,
