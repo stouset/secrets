1188847a797766fc6af05d27fe32e133c9adcc7d
Stephen Touset <stephen@touset.org>
2019-07-09 14:30:15 -0700

    Ensure ctest isn't linked in release builds

--- build.rs
+++ build.rs
@@ -1,25 +1,71 @@
-use ctest::TestGenerator;
 use pkg_config::{Config as PkgConfig, Library, Error};
+
 use std::env;
+use std::fmt;
+
+#[derive(PartialEq, Eq)]
+enum Profile {
+    Debug,
+    Coverage,
+    Release,
+}
+
+impl Profile {
+    fn infer() -> Self {
+        let profile = match (
+            env::var("COVERAGE").is_ok(),
+            env::var("PROFILE").as_ref().map(String::as_str),
+        ) {
+            (true, _)              => Profile::Coverage,
+            (false, Ok("release")) => Profile::Release,
+            (false, Ok("debug"))   => Profile::Debug,
+
+            // unknown profile
+            (false, Ok(profile)) => {
+                println!("cargo:warning=\"unknown profile {}, defaulting to debug\"", profile);
+                Profile::Debug
+            },
+
+            // this should only happen if we typo'd an env key
+            (false, Err(err)) => {
+                panic!("build.rs failed to determine profile: {}", err);
+            }
+        };
+
+        println!("cargo:rerun-if-env-changed=COVERAGE");
+        println!("cargo:rerun-if-env-changed=PROFILE");
+        println!("cargo:rustc-cfg=profile=\"{}\"", profile);
+
+        profile
+    }
+}
+
+impl fmt::Display for Profile {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Profile::Debug    => write!(f, "debug"),
+            Profile::Coverage => write!(f, "coverage"),
+            Profile::Release  => write!(f, "release"),
+        }
+    }
+}
 
 fn main() {
-    TestGenerator::new()
-        .header("sodium.h")
-        .generate("src/ffi/sodium.rs", "sodium_ctest.rs");
+    let profile = Profile::infer();
 
+    if profile != Profile::Release {
+        ctest::TestGenerator::new()
+            .header("sodium.h")
+            .generate("src/ffi/sodium.rs", "sodium_ctest.rs");
+    }
+
+    // 1.0.8 was chosen (IIRC) because this is when the garbage byte
+    // value was fixed at 0xdb
     if link("libsodium", "1.0.8").is_none() {
         // if pkg-config is disabled or failed to run, try and link
         // naïvely
         println!("cargo:rustc-link-lib=dylib=sodium");
     };
-
-    if env::var("COVERAGE").is_ok() {
-        println!("cargo:rustc-cfg=profile=\"coverage\"");
-    } else if env::var("PROFILE") == Ok("release".into()) {
-        println!("cargo:rustc-cfg=profile=\"release\"");
-    } else {
-        println!("cargo:rustc-cfg=profile=\"dev\"");
-    }
 }
 
 fn link(name: &str, version: &str) -> Option<Library> {
